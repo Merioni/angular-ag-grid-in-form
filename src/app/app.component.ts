690fb16b328078dec7da0f51cf387d110809f7c5
Merioni <merioni536@gmail.com>
2021-05-03 21:40:16 +0200

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { GridApi } from "ag-grid-community";
 
 @Component({
@@ -8,17 +9,44 @@ import { GridApi } from "ag-grid-community";
 })
 export class AppComponent {
   public rowData = [
-    {col1: 'abc', col2: 'def', col3: 'ghi'},
-    {col1: 'def', col2: 'ghi', col3: 'jkl'},
-    {col1: 'ghi', col2: 'jkl', col3: 'mno'},
+    { col1: "abc", col2: "def", col3: "ghi" },
+    { col1: "def", col2: "ghi", col3: "jkl" },
+    { col1: "ghi", col2: "jkl", col3: "mno" }
   ];
-  public columnDefs = [
-    {field: 'col1'},
-    {field: 'col2'},
-    {field: 'col3'},
-  ]
+  public columnDefs = [{ field: "col1" }, { field: "col2" }, { field: "col3" }];
+
+  public formGroup: FormGroup;
+  public form: FormArray;
+
   private api: GridApi;
+
+  constructor(private readonly fb: FormBuilder) {
+    this.formGroup = this.fb.group({
+      name: ["", Validators.required],
+      table: this.form
+    });
+    this.form = this.fb.array([]);
+  }
+
+  refreshTable() {
+    this.form.clear();
+    this.rowData.forEach(row => {
+      this.form.push(
+        this.fb.group({
+          col1: [row.col1, Validators.required],
+          col2: [row.col2],
+          col3: [row.col3, Validators.minLength(2)]
+        })
+      );
+    });
+  }
+
   onGridReady(params) {
     this.api = params.api;
+    this.refreshTable();
+  }
+
+  onSubmit(params) {
+    console.log("submit pressed", params);
   }
 }
